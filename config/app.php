bda0451d64c9042a2339145e137b3a608dc4b396
Francisco Roruqe Júnior <francisco.roque@codingtech.com.br>
2018-09-07 13:44:34 -0400

    Adicionando o Repository ao projeto e refatorando as Model de Categoria, Livros e Usuários

--- config/app.php
+++ config/app.php
@@ -170,7 +170,7 @@ return [
         //Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Bootstrapper\BootstrapperL5ServiceProvider::class,
-
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
         //
 
         /*
@@ -181,6 +181,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        \App\Providers\RepositoryServiceProvider::class,
 
     ],
 
